a537d359d2ff4cf82151761fcf41b40ffc9b71b0
gui777x <guizin.clementino@gmail.com>
2024-11-28 12:26:43 -0300

    AppDbContext

---
+++ EtecShopAPI/Models/Produto.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EtecShopAPI.Models;
+
+[Table("Produto")]
+public class Produto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public int Nome { get; set; }
+
+
+    [StringLength(1000)]
+    public string Descricao {get; set; }
+
+    [Required]
+    public int Estoque { get; set; }
+
+    [Required]
+    [Column(TypeName = "decima(8,2)")]
+    [DisplayFormat(DataFormatString = "{0:N2}")]
+    public decimal ValorCusto {get; set;}
+
+    [Required]
+    [Column(TypeName = "decima(8,2)")]
+    [DisplayFormat(DataFormatString = "{0:N2}")]
+    public decimal ValorVenda {get; set;}
+}
